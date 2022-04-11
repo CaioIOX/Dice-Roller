8295535a8900b83ff40fe18c7a62bfbf6b966851
Caio Ícaro <zeroaz819@gmail.com>
2022-04-11 01:02:35 -0300

    Initial commit

---
+++ app/src/main/java/com/caioiox/diceroller/Dice.kt
@@ -0,0 +1,5 @@
+package com.caioiox.diceroller
+
+class Dice (diceScope : Int) {
+        var dice = (1..diceScope).random()
+}
---
+++ app/src/main/java/com/caioiox/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.caioiox.diceroller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.caioiox.diceroller.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.setOnClickListener { rollDice() }
+//        binding.rollButton.setOnClickListener {
+//            Toast.makeText(this, "Dado rolado", Toast.LENGTH_SHORT).show() }
+
+
+    }
+    private fun rollDice(){
+        val drawableResource = when (Dice(6).dice){
+            1-> binding.rollImage.setImageResource(R.drawable.dice_1)
+            2-> binding.rollImage.setImageResource(R.drawable.dice_2)
+            3-> binding.rollImage.setImageResource(R.drawable.dice_3)
+            4-> binding.rollImage.setImageResource(R.drawable.dice_4)
+            5-> binding.rollImage.setImageResource(R.drawable.dice_5)
+            else-> binding.rollImage.setImageResource(R.drawable.dice_6)
+        }
+        return drawableResource
+//        binding.rollResult.text = Dice(6).dice.toString()
+    }
+}
